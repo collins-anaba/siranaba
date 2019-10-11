bf6318f9e03bd71bfac27024ab3ebe62c53ef7c6
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-10-11 09:38:22 -0500

    server updated

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Review from './components/Reviews/Reviews';
+import Reviews from './components/Reviews/Reviews';
 import News from './components/News/News';
-import Navbar from './components/NavBar.js/Navbar';
+import Navbar from './components/NavBar/Navbar';
+import Admin from './components/Admin/Admin';
+
+export default (
+    <Switch>
+        <Route exact path='/'  component={Home}/>
+        <Route path='/News' exact component={News}/>
+        <Route path='/Reviews' exact component={Reviews}/>
+        <Route path='/NavBar' exact component={Navbar}/>
+        <Route path='/Admin' exact component={Admin}/>
+
+    </Switch>
+)
