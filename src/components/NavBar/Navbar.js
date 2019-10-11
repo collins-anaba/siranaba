24b669e02928b1afaff0a39f0bbb8f71bb3d9e0a
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-10-11 17:44:52 -0500

    updated navbar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React,  {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 
-export default class Navbar extends component {
+export default class Navbar extends Component {
     constructor() {
-        super()
-        
+        super();
+        this.state = {
+            menuStatus: 'closed'
+        }
+    }
+        handleClick = ()=>{
+            if(this.state.menuStatus === 'open'){
+                this.setState({
+                    menuStatus: 'closed'
+                })
+            } else {
+                this.setState ({
+                    menuStatus: 'open'
+                })
+            }
+        }
+    render(){
+        return (
+            <div className='bar'>
+                <nav>
+                
+                <div className='mobile-container' onclick={()=> this.handleclick()}>
+                  <img className='Hamburger-Image' 
+                 src="https://cdn2.iconfinder.com/data/icons/mobile-banking-ver-3a/100/1-48-512.png"
+                 alt='menu-button'/>  
+                 </div>
+
+                <div>
+                 <ul>
+                <Link>Home</Link>
+                <Link>Reviews</Link>
+                <Link>News</Link>
+                <Link>Contact</Link>
+                <Link>Sign In</Link>
+                 </ul>
+                </div>
+                
+                </nav>
+            </div>
+        )
     }
 }
