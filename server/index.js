f8b6098c0ecee875c4c496d98e5be7381612422f
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-10-15 11:01:01 -0500

    added endpoints news, reviews, comments,and auth to index.js

--- server/index.js
+++ server/index.js
@@ -22,5 +22,29 @@ massive(process.env.CONNECTION_STRING).then( db =>{
     app.set('db',db);
     console.log('Database Connected')
 })
+
+//NewsEndpoints
+app.get('/api/news', newsController.getAll)
+app.post('/api/news', newsController.create)
+app.put('/api/news: title', newsController.update)
+app.delete('/api/news:title', newsController.delete)
+//reviewsEndpoints
+app.get('/api/reviews', reviewsController.getAll)
+app.post('/api/reviews', reviewsController.create)
+app.put('/api/reviews: title', newsController.update)
+app.delete('/api/reviews:title', newsController.delete)
+//commentEndpoints
+app.get('/api/comments/', commentsController.getAll)
+app.post('/api/comments/', commentsController.create)
+app.put('/api/comments: title/', commentsController.update)
+app.delete('/api/comments: title/', commentsController.delete)
+//authEndpoints
+app.get('/api/login', authController.loginUser)
+app.delete('/api/logout',authController.logOut)
+app.post('/api/admin', authController.loginAdmin)
+app.post('/api/Register', authController.register)
+app.post('/api/userUpdate', authController.updateUser)
+
+
 //port
 app.listen(5000, () => console.log('Listening on Port 5000'))
