883721ae4de5ddbd742759d9c31cb1309dc65fc5
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-10-28 10:58:53 -0500

    updated admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -6,15 +6,11 @@ export default class Admin extends Component {
 constructor(){
     super()
     this.state = {
-        article: [],
-        news: { 
+        article: {},
             newsArticle : '',
             newsImage:'',
-        },
-        reviews: {
             reviewsArticle: '',
             reviewsImage: ''
-        }
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -23,26 +19,59 @@ handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
-handleSubmit (){
-    axios.put('/api/news/${this.state.news}', {
-        newsArticle: this.state.newsArticle,
-        newsImage: this.state.newsImage,
-        reviewsArticle: this.state.news.reviewsArticle,
-        reviewsImage: this.state.reviewsArticle
+getNewsArticle(){
+    let newsArticle = this.props.match.params.id;
+    axios.get()
+}
+
+delete(id) {
+    let newsArticle 
+}
+
+addNewsArticle(newNewsArticle){
+axios.post(`/api/news/`, {news}).then(res => {
+    this.props.history.push('/')
+}).catch(err => console.log(err))
+}
+
+onSubmit (e){
+    const newNewsArticle = {
+        newsArticle: this.refs.newsArticle.value,
+        newImage: this.refs.newsImage.value
     }
-    ).then(res => {
-        this.setState({
-article: res.data
-        });
-    });
+    this.addNewsArticle(newNewsArticle);
+    e.preventDefault();
 }
 
 render () {
     return (
          <div>
-             <button>Add</button>
-             <button>Edit</button>
-             <button>Delete</button>
+             <form onSubmit={this.onSubmit.bind(this)}>
+                 <div className='input-field-newsImage'>
+                     <input type='image' name='newImage' ref='newsImage'/>
+                     <label htmlFor='newsImage'/>
+                 </div>
+                 <div className='input-field-newsCaption'>
+                     <input type='text' name='newsArticle' ref='newsArticle'/>
+                     <label htmlFor='text'/>
+                     <button type='submit' value='add'>Add news article</button>
+                 </div>
+                 <br/>
+                 <div className='input-field-reviewsImage'>
+                     <input type='image' name='image' ref='news'/>
+                     <label htmlFor='image'/>
+                 </div>
+                 <div className='input-field-reviewsCaption'>
+                     <input type='text' name='news' ref='news'/>
+                     <label htmlFor='text'/>
+                     <button type='submit' value='add'>Add review article</button>
+                 </div>
+             </form>
+
+
+             <button className="edit-article-button" onClick={() => {this.setState({ showEdit: !this.state.showEdit }); }}>Edit</button>
+             
+             <button className='delete-article-button' onClick={this.onDelete.bind(this)}>Delete</button>
          </div>
     )
 }
