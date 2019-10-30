8f0df4b9d7b00d23ba80f8a19194b0adc3035746
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-10-29 22:56:40 -0500

    admin updated

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -6,35 +6,33 @@ export default class Admin extends Component {
 constructor(){
     super()
     this.state = {
-        article: {},
             newsArticle : '',
             newsImage:'',
             reviewsArticle: '',
             reviewsImage: ''
     }
-    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.onSubmit1 = this.onSubmit1.bind(this);
+    this.onSubmit2 = this.onSubmit2.bind(this);
+
 }
 handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
-getNewsArticle(){
-    let newsArticle = this.props.match.params.id;
-    axios.get()
-}
 
 delete(id) {
     let newsArticle 
 }
 
-addNewsArticle(newNewsArticle){
-axios.post(`/api/news/`, {news}).then(res => {
+addArticle(e){
+    const {newsArticle,newsImage,reviewsArticle,reviewsImage}= this.state;
+axios.post(`/api/news/`, {newsArticle,newsImage,reviewsArticle,reviewsImage}).then(res => {
     this.props.history.push('/')
 }).catch(err => console.log(err))
 }
 
-onSubmit (e){
+onSubmit1 (e){
     const newNewsArticle = {
         newsArticle: this.refs.newsArticle.value,
         newImage: this.refs.newsImage.value
@@ -43,10 +41,20 @@ onSubmit (e){
     e.preventDefault();
 }
 
+onSubmit2 (e){
+    console.log('alert')
+    const newReviewsArticle = {
+        reviewsArticle: this.refs.reviewsArticle.value,
+        reviewImage: this.refs.reviewImage.value
+    }
+    this.addArticle(newReviewsArticle);
+    e.preventDefault();
+}
+
 render () {
     return (
          <div>
-             <form onSubmit={this.onSubmit.bind(this)}>
+             <form onSubmit1={this.onSubmit1.bind(this)}>
                  <div className='input-field-newsImage'>
                      <input type='image' name='newImage' ref='newsImage'/>
                      <label htmlFor='newsImage'/>
@@ -54,9 +62,11 @@ render () {
                  <div className='input-field-newsCaption'>
                      <input type='text' name='newsArticle' ref='newsArticle'/>
                      <label htmlFor='text'/>
-                     <button type='submit' value='add'>Add news article</button>
+                     <button onClick={this.addArticle}>Add news article</button>
                  </div>
+             </form>
                  <br/>
+             <form onSubmit2={this.onSubmit1.bind(this)}>
                  <div className='input-field-reviewsImage'>
                      <input type='image' name='image' ref='news'/>
                      <label htmlFor='image'/>
@@ -64,14 +74,14 @@ render () {
                  <div className='input-field-reviewsCaption'>
                      <input type='text' name='news' ref='news'/>
                      <label htmlFor='text'/>
-                     <button type='submit' value='add'>Add review article</button>
+                     <button onClick={this.addArticle}>Add review article</button>
                  </div>
              </form>
 
 
              <button className="edit-article-button" onClick={() => {this.setState({ showEdit: !this.state.showEdit }); }}>Edit</button>
              
-             <button className='delete-article-button' onClick={this.onDelete.bind(this)}>Delete</button>
+             {/* <button className='delete-article-button' onClick={this.onDelete.bind(this)}>Delete</button> */}
          </div>
     )
 }
